776dc1a019a98cf8e83739441d4cfb476165c825
dalo2903 <dalo2903@gmail.com>
2018-11-19 09:57:47 +0700

    Update classify flow

--- Server.py
+++ Server.py
@@ -1,29 +1,29 @@
 #import numpy as np
-from flask import Flask, request
+from flask import Flask, request, abort, Response
 import re
 import sys
 import os
 import cv2
 import re
 import requests
-import base64
 import numpy as np
 import keras_video_classifier
 import json
 import asyncio
-import tensorflow as tf
-from multiprocessing import Process
-from keras import backend as K
-
+# import tensorflow as tf
+# from keras import backend as K
+SECRET_KEY = 'hK0Oeiy2zft0ZZtbmaawNW5zT0Ebcybn'
+ADMIN_ID = '5b86b4a8a96c6a000446705b'
+GODSEYE_URL = 'http://localhost/api/visual-data/classified'
 
 def patch_path(path):
     return os.path.join(os.path.dirname(__file__), path)
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'hello'
+# @app.route('/')
+# def hello():
+#     return 'hello'
 
 # @app.route('/download')
 # def download_test():
@@ -33,9 +33,33 @@ def hello():
 #     url = request.args.get('url')
 #     file_path = download_file(url, download_path)
 #     return file_path
+# @app.route('/test')
+# def test():
+#     img = cv2.imread('45173636_284087462439146_441652943041593344_n.jpg')
+#     name = "hahaha.jpg"
+#     success, buffer = cv2.imencode('.jpg',img)
+#     face_images = {}
+#     data = {}
+#     if success:
+#         # cv2.imshow("img",img)
+#         # cv2.waitKey(0)
+#         # print(success,buffer)
+#         face_images[name] = buffer.tostring()
+#     response = json.dumps(data)
+
+#     p = Process(target=send_images, args = (face_images, data,))
+#     p.start()
+#     return "done"
+#     p.join()
 
 @app.route('/classify')
 def classify_video():
+    secret = request.args.get('secret')
+    if(secret != SECRET_KEY):
+        error = {}
+        error['code'] = 403
+        error['message'] = 'Invalid secret key'
+        return json.dumps(error),403
     download_path = patch_path('downloads')
     if not os.path.exists(download_path):
         os.makedirs(download_path)    
@@ -44,34 +68,31 @@ def classify_video():
     print(file_path)
     result, face_images = classify(file_path)
     data = {}
+    data['id'] = ADMIN_ID
     data['video_id'] = str(url[url.rfind("/")+1:]).split('.')[0]
     data['result'] = result
     data['images'] = len(face_images)
     for name, img in face_images.items():
         success, buffer = cv2.imencode('.jpg',img)
         if success:
-            cv2.imshow("img",img)
+            # cv2.imshow("img",img)
             # cv2.waitKey(0)
             # print(success,buffer)
             face_images[name] = buffer
-    response = json.dumps(data)
-
-    p = Process(target=send_images, args = (face_images, data,))
-    p.start()
+    # p = Process(target=send_images, args = (face_images, data,))
+    # p.start()
+    send_images(face_images, data)
     os.remove(file_path)
-    # json.dumps(np.array(face_images).tolist())
-    # K.clear_session()
-    # tf.reset_default_graph()
-    return response
+    return json.dumps(data)
 
 def send_images(images, data):
-    url = 'http://localhost/api/visual-data/'
     files = []
     for k, v in images.items():        
-        files.append((k,base64.b64encode(v)))
-    print('Sending images to ' + url)
-    r = requests.post(url,files=files, data=data)
+        files.append((k,v))
+    print('Sending images to ' + GODSEYE_URL)
+    r = requests.post(GODSEYE_URL,files=files, data=data)
     print(r)
+    return
 
 def classify(file_path):
     # sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
